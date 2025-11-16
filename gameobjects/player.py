0d34dbaa6cf658dbe21f351c6d1532378b85730b
mathieu <mathieu.bonithon@gmail.com>
2025-11-15 20:39:54 -0500

    animation state + idle

--- gameobjects/player.py
+++ gameobjects/player.py
@@ -15,7 +15,11 @@ class Player:
             'walk': [
                 pygame.image.load(f'assets/sprites/player/player{index}_walk_0.png').convert_alpha(),
                 pygame.image.load(f'assets/sprites/player/player{index}_walk_1.png').convert_alpha(),
-                pygame.image.load(f'assets/sprites/player/player{index}_walk_2.png').convert_alpha()]
+                pygame.image.load(f'assets/sprites/player/player{index}_walk_2.png').convert_alpha()],
+            'idle': [
+                pygame.image.load(f'assets/sprites/player/player{index}_idle_0.png').convert_alpha(),
+                pygame.image.load(f'assets/sprites/player/player{index}_idle_0.png').convert_alpha(),
+                pygame.image.load(f'assets/sprites/player/player{index}_idle_1.png').convert_alpha()]
         }
 
         # animation
@@ -23,8 +27,9 @@ class Player:
         self.animation_timer = 0
         self.animation_speed = .15 # seconds
         self.looking_right = True
+        self.previous_pos = (x, y)
 
-        self.state = 'walk'
+        self.state = 'idle'
         surface = self.animations[self.state][0]
 
         self.Surface = surface
@@ -46,12 +51,20 @@ class Player:
     def update(self, dt: float):
         self.BlinkingComponent.update(dt)
 
+        if self.previous_pos == self.Rect.topleft:
+            self.state = 'idle'
+        else:
+            self.state = 'walk'
+
         # animation
         self.animation_timer += dt
         if self.animation_timer >= self.animation_speed:
             self.animation_timer %= self.animation_speed
 
             self.frame_index = (self.frame_index + 1) % len(self.animations[self.state])
+
+        self.previous_pos = self.Rect.topleft
+
 
 
     def draw(self, screen):
@@ -62,30 +75,24 @@ class Player:
             screen.blit(img, self.Rect.topleft)
 
     def MoveX(self, value, obstacles: list[Rect]):
-        #self.Rect.x += self.Speed * value
         MoveAndCollide(self.Rect, self.Speed * value, 0, obstacles)
         self.looking_right = value > 0
 
     def MoveY(self, value, obstacles: list[Rect]):
-        #self.Rect.y += self.Speed * value
         MoveAndCollide(self.Rect, 0, self.Speed * value, obstacles)
 
     def MoveLeft(self, obstacles: list[Rect], ratio: float = 1):
-        #self.Rect.x -= int(self.Speed * ratio)
         MoveAndCollide(self.Rect, int(self.Speed * ratio) * -1, 0, obstacles)
         self.looking_right = False
 
     def MoveRight(self, obstacles: list[Rect], ratio: float = 1):
-        #self.Rect.x += int(self.Speed * ratio)
         MoveAndCollide(self.Rect, int(self.Speed * ratio), 0, obstacles)
         self.looking_right = True
 
     def MoveUp(self, obstacles: list[Rect], ratio: float = 1):
-        #self.Rect.y -= int(self.Speed * ratio)
         MoveAndCollide(self.Rect, 0, int(self.Speed * ratio) * -1, obstacles)
 
     def MoveDown(self, obstacles: list[Rect], ratio: float = 1):
-        #self.Rect.y += int(self.Speed * ratio)
         MoveAndCollide(self.Rect, 0, int(self.Speed * ratio), obstacles)
 
     def ReceiveDamage(self):
