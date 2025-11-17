ded14555d11efa4adbf25b50cfbce9a564c50c95
mathieu <mathieu.bonithon@gmail.com>
2025-11-16 23:35:11 -0500

    TitleBG (first version)

--- gamestates/titleState.py
+++ gamestates/titleState.py
@@ -6,6 +6,7 @@ from pygame.locals import *
 class TitleState(GameState):
     def enter(self):
         print("Entered Title State")
+        self.TitleSurface = pygame.image.load('assets/sprites/environment/backgrounds/titleBG.png').convert_alpha()
 
     def handle_events(self, events):
         for event in events:
@@ -20,15 +21,17 @@ class TitleState(GameState):
     def draw(self, screen):
         screen.fill((30, 30, 60))
 
-        title_font = pygame.font.SysFont(None, 64)
-        title_text = title_font.render("TEMPLATE GAME", True, (255, 255, 255))
-        screen.blit(title_text, (100, 100))
+        screen.blit(self.TitleSurface, (0,0))
+
+        # title_font = pygame.font.SysFont(None, 64)
+        # title_text = title_font.render("TEMPLATE GAME", True, (255, 255, 255))
+        # screen.blit(title_text, (100, 100))
 
 
-        subtitle_font = pygame.font.SysFont(None, 32)
+        subtitle_font = pygame.font.SysFont(None, 96)
         subtitle_text = subtitle_font.render("Press Space to start", True, (255, 255, 255))
-        screen.blit(subtitle_text, (100, 300))
+        screen.blit(subtitle_text, (600, 800))
 
-        highscore_font = pygame.font.SysFont(None, 24)
-        score_text = highscore_font.render(f'High score: {self.game.game_data['highscore']}', True, (255, 255, 255))
-        screen.blit(score_text, (500, 440))
+        # highscore_font = pygame.font.SysFont(None, 24)
+        # score_text = highscore_font.render(f'High score: {self.game.game_data['highscore']}', True, (255, 255, 255))
+        # screen.blit(score_text, (500, 440))
--- utils/parameters.py
+++ utils/parameters.py
@@ -4,4 +4,4 @@ NUMBER_OF_ENEMIES_TO_SPAWN = 1 #3
 
 
 # "Splash","Title","Gameplay","Action","Rebind","GameOver""Elevator"
-STARTING_STATE = 'Action'
+STARTING_STATE = 'Title'
